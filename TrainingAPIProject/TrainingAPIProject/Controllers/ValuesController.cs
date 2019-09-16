283c43cd6361581a02813543018f69f6392d2a41
saikumarreddy1994 <44068822+saikumarreddy1994@users.noreply.github.com>
2019-09-16 10:55:36 +0530

    test2
    test2

--- TrainingAPIProject/TrainingAPIProject/Controllers/ValuesController.cs
+++ TrainingAPIProject/TrainingAPIProject/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace TrainingAPIProject.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "sss", "value2" };
         }
 
         // GET api/values/5
